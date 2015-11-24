33d53a893cf16d3c1aed568fe3679dc01f94439a
TranceLove <airwave209gt@gmail.com>
2015-11-24 16:38:14 +0800

    Initial commit

--- AFNetworking-studyTests/AFNetworking_studyTests.m
+++ AFNetworking-studyTests/AFNetworking_studyTests.m
@@ -7,33 +7,76 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "AFHTTPSessionManager.h"
+#import "AFURLResponseSerialization.h"
 
 @interface AFNetworking_studyTests : XCTestCase
-
+@property (readwrite,nonatomic,strong) AFHTTPSessionManager *manager;
 @end
+
+NSString *urlPrefix = @"http://httpbin.org";
 
 @implementation AFNetworking_studyTests
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    self.manager = [[AFHTTPSessionManager alloc] initWithBaseURL:[NSURL URLWithString:@"http://httpbin.org"]];
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
+    self.manager = nil;
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
+- (void)testSimpleGet {
+    XCTestExpectation *expect = [self expectationWithDescription:@"Test expectations"];
+    //Response serializer is required or task will fail with unacceptable content type text/html
+    self.manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    NSURLSessionDataTask *task = [self.manager GET:@"/"
+           parameters:nil
+              success:^(NSURLSessionDataTask *task, id responseObject){
+                  XCTAssertTrue(YES);
+                  NSData *responseData = responseObject;
+                  NSString *response = [[NSString alloc]initWithData:responseData encoding:NSUTF8StringEncoding];
+                  XCTAssertNotNil(response);
+                  [expect fulfill];
+              }
+            failure:^(NSURLSessionDataTask *task, NSError *error){
+                NSLog(@"%@", error.localizedDescription);
+                XCTAssertNil(error);
+            }];
+    
+    [task resume];
+    
+    [self waitForExpectationsWithTimeout:5.0 handler:^(NSError *error){
+        if(error)
+            NSLog(@"Timed out with error: %@", error);
     }];
 }
+
+- (void)testSimpleGetJson {
+    XCTestExpectation *expect = [self expectationWithDescription:@"Test expectations"];
+    //Response serializer is required or task will fail with unacceptable content type text/html
+    self.manager.responseSerializer = [AFJSONResponseSerializer serializer];
+   [self.manager GET:@"/get"
+          parameters:nil
+             success:^(NSURLSessionDataTask *task, NSDictionary *response){
+                XCTAssertNotNil(response);
+                XCTAssertEqualObjects([response objectForKey:@"url"], @"http://httpbin.org/get");
+                [expect fulfill];
+             }
+             failure:^(NSURLSessionDataTask *task, NSError *error){
+                 NSLog(@"%@", error.localizedDescription);
+                 XCTAssertNil(error);
+             }
+    ];
+    
+
+    [self waitForExpectationsWithTimeout:5.0 handler:^(NSError *error){
+        if(error)
+            NSLog(@"Timed out with error: %@", error);
+    }];
+}
+
 
 @end
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+# use_frameworks!
+
+target 'AFNetworking-study' do
+
+end
+
+target 'AFNetworking-studyTests' do
+    pod 'AFNetworking', '~>2.0'
+end
